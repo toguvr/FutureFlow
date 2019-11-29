a45775a6bf62c7be32fb7c183303319674ff1944
toguvr <augustotf93@gmail.com>
2019-11-29 20:27:33 -0300

    subindo com boilerplate atualizado

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -4,14 +4,12 @@ import { push } from "connected-react-router";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import styled from "styled-components";
+import QuestionCard from "../../Components/QuestionCard";
 
 const LoginWrapper = styled.form`
   width: 100%;
   height: 100vh;
-  gap: 10px;
-  place-content: center;
   justify-items: center;
-  display: grid;
 `;
 
 class LoginPage extends Component {
@@ -34,21 +32,7 @@ class LoginPage extends Component {
 
     return (
       <LoginWrapper>
-        <TextField
-          onChange={this.handleFieldChange}
-          name="email"
-          type="email"
-          label="E-mail"
-          value={email}
-        />
-        <TextField
-          onChange={this.handleFieldChange}
-          name="password"
-          type="password"
-          label="Password"
-          value={password}
-        />
-        <Button>Login</Button>
+        <QuestionCard/>
       </LoginWrapper>
     );
   }
